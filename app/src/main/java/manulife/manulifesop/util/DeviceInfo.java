3028000d8fdf4d7b7d6fa5281170f790918860a2
tri.nguyen833@gmail.com <tri.nguyen833@gmail.com>
2018-01-29 15:27:43 +0700

    get DEVICE_NAME, NETWORK_TYPE generate checksum string

--- app/src/main/java/manulife/manulifesop/util/DeviceInfo.java
+++ app/src/main/java/manulife/manulifesop/util/DeviceInfo.java
@@ -2,6 +2,8 @@ package manulife.manulifesop.util;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -19,6 +21,8 @@ public class DeviceInfo {
 	public static String ANDROID_OS_VERSION;
 	public static String MODEL_NUMBER;
 	public static String IP_ADDRESS;
+	public static String DEVICE_NAME;
+	public static String NETWORK_TYPE;
 
 	private TelephonyManager mTelephonyMgr;
 
@@ -29,6 +33,71 @@ public class DeviceInfo {
 		ANDROID_SDK_VERSION = getDeviceAndroidSDKVersion();
 		MODEL_NUMBER = getDeviceModelNumber();
 		ANDROID_OS_VERSION = getDeviceAndroidOSVersion();
+		DEVICE_NAME = getDeviceName();
+		NETWORK_TYPE = getNetworkClass(mContext);
+	}
+
+	/**
+	 * get net work type
+	 * */
+	public static String getNetworkClass(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+		if(info==null || !info.isConnected())
+			return "-"; //not connected
+		if(info.getType() == ConnectivityManager.TYPE_WIFI)
+			return "WIFI";
+		if(info.getType() == ConnectivityManager.TYPE_MOBILE){
+			int networkType = info.getSubtype();
+			switch (networkType) {
+				case TelephonyManager.NETWORK_TYPE_GPRS:
+				case TelephonyManager.NETWORK_TYPE_EDGE:
+				case TelephonyManager.NETWORK_TYPE_CDMA:
+				case TelephonyManager.NETWORK_TYPE_1xRTT:
+				case TelephonyManager.NETWORK_TYPE_IDEN: //api<8 : replace by 11
+					return "2G";
+				case TelephonyManager.NETWORK_TYPE_UMTS:
+				case TelephonyManager.NETWORK_TYPE_EVDO_0:
+				case TelephonyManager.NETWORK_TYPE_EVDO_A:
+				case TelephonyManager.NETWORK_TYPE_HSDPA:
+				case TelephonyManager.NETWORK_TYPE_HSUPA:
+				case TelephonyManager.NETWORK_TYPE_HSPA:
+				case TelephonyManager.NETWORK_TYPE_EVDO_B: //api<9 : replace by 14
+				case TelephonyManager.NETWORK_TYPE_EHRPD:  //api<11 : replace by 12
+				case TelephonyManager.NETWORK_TYPE_HSPAP:  //api<13 : replace by 15
+					return "3G";
+				case TelephonyManager.NETWORK_TYPE_LTE:    //api<11 : replace by 13
+					return "4G";
+				default:
+					return "?";
+			}
+		}
+		return "?";
+	}
+
+	/**
+	 * get device name
+	 * */
+	public String getDeviceName() {
+		String manufacturer = Build.MANUFACTURER;
+		String model = Build.MODEL;
+		if (model.startsWith(manufacturer)) {
+			return capitalize(model);
+		} else {
+			return capitalize(manufacturer) + " " + model;
+		}
+	}
+
+	private String capitalize(String s) {
+		if (s == null || s.length() == 0) {
+			return "";
+		}
+		char first = s.charAt(0);
+		if (Character.isUpperCase(first)) {
+			return s;
+		} else {
+			return Character.toUpperCase(first) + s.substring(1);
+		}
 	}
 
 	/**
--- app/src/main/java/manulife/manulifesop/util/Utils.java
+++ app/src/main/java/manulife/manulifesop/util/Utils.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.provider.SyncStateContract;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 import manulife.manulifesop.ProjectApplication;
 
@@ -14,6 +19,8 @@ import manulife.manulifesop.ProjectApplication;
  * Created by trinm on 12/01/2018.
  */
 public class Utils {
+
+    private static String SECRETKEY = "abc";
 
     public static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager =
@@ -29,5 +36,40 @@ public class Utils {
                 .getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    public static String getSignature(String data)
+    {
+        String base64 = Base64(data) + SECRETKEY;
+        String md5 = MD5(base64);
+        return md5.toLowerCase(Locale.getDefault());
+    }
+
+    private static String Base64(String data)
+    {
+        return android.util.Base64.encodeToString(data.getBytes(),android.util.Base64.NO_WRAP);
+    }
+    private static final String MD5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
