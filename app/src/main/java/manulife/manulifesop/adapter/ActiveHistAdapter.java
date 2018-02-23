2f174229ed21f7410b54a788da2ad7c6f6fbdb7d
tri.nguyen833@gmail.com <tri.nguyen833@gmail.com>
2018-02-23 14:10:25 +0700

    add list active history in dashboard fa

--- app/src/main/java/manulife/manulifesop/adapter/ActiveHistAdapter.java
+++ app/src/main/java/manulife/manulifesop/adapter/ActiveHistAdapter.java
@@ -3,12 +3,16 @@ package manulife.manulifesop.adapter;
 import android.content.Context;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.makeramen.roundedimageview.RoundedImageView;
 
@@ -37,6 +41,7 @@ public class ActiveHistAdapter extends RecyclerView.Adapter<ActiveHistAdapter.Vi
 
     /**
      * Create view holder for recycle view
+     *
      * @param parent
      * @param viewType
      * @return
@@ -52,22 +57,55 @@ public class ActiveHistAdapter extends RecyclerView.Adapter<ActiveHistAdapter.Vi
 
     /**
      * Set data for recycle view
+     *
      * @param holder
      * @param position
      */
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         final ActiveHistFA object = mListObject.get(position);
-        if(object.getAvatar() != null && object.getAvatar().length() > 1){
+        if (object.getAvatar() != null && object.getAvatar().length() > 1) {
 
         }
         holder.txtTitle.setText(object.getTitle());
         holder.txtContent.setText(object.getContent());
         holder.userAvatar.setBorderColor(mContext.getResources().getColor(R.color.colorSecond));
+        holder.layoutRoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(mContext, holder.menuRight);
+                popup.inflate(R.menu.option_menu_active_hist);
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu1:
+                                //handle menu1 click
+                                Toast.makeText(mContext, "menu 1", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.menu2:
+                                //handle menu2 click
+                                Toast.makeText(mContext, "menu 2", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.menu3:
+                                Toast.makeText(mContext, "menu 3", Toast.LENGTH_SHORT).show();
+                                //handle menu3 click
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                //displaying the popup
+                popup.show();
+            }
+        });
     }
 
     /**
      * Get number of item in List
+     *
      * @return
      */
     @Override
@@ -93,11 +131,14 @@ public class ActiveHistAdapter extends RecyclerView.Adapter<ActiveHistAdapter.Vi
         TextView txtContent;
         @BindView(R.id.menu_right)
         ImageButton menuRight;
+        @BindView(R.id.layout_root)
+        RelativeLayout layoutRoot;
+
 
 
         public ViewHolder(View itemView) {
             super(itemView);
-            ButterKnife.bind(this,itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
